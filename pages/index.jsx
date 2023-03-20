0cca24691ee84f74ac9037383390645a9cb53e73
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2023-03-19 23:22:45 -1000

    start basic card on Login page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 // import Image from "next/image"
-import styles from "@/styles/Home.module.css"
+// import styles from "@/styles/Home.module.css"
 
 export default function Home() {
 	return (
@@ -9,8 +9,46 @@ export default function Home() {
 				<title>Creative Juice</title>
 				<meta name="description" content="Creative Juice" />
 			</Head>
-			<main className={`${styles.main} bg-primary`}>
-				<p>hi</p>
+			<main className="p-6 bg-primary flex min-h-screen">
+				<div className="container m-auto max-w-xl">
+					<div className="card min-w-full">
+						<p className="text-3xl">log in</p>
+						<br />
+						<form>
+							<label htmlFor="email">
+								<p className="text-sm text-green-800">email</p>
+								{/* TODO: text-green-800 (and all Tailwind colors) don't work */}
+							</label>
+
+							{/* TODO: input doesn't show up! */}
+							<input
+								type="email"
+								name="email"
+								id="email"
+								className="rounded sm"
+							/>
+							<br />
+							<br />
+							<label htmlFor="password">
+								<p className="text-sm text-green-800">
+									password
+								</p>
+								{/* <input
+									type="password"
+									name="password"
+									id="password"
+								/> */}
+							</label>
+
+							<button
+								className="bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r"
+								type="submit"
+							>
+								Sign Up
+							</button>
+						</form>
+					</div>
+				</div>
 			</main>
 		</>
 	)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+const colors = require("tailwindcss/colors")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: "jit",
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		colors: {
+			black: colors.black,
+			white: colors.white,
+			gray: colors.gray,
+
 			primary: "#5C38F5",
 		},
 		extend: {},
